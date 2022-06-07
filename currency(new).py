cb57c2a61207eae2f656fe40199376f57f09b6d9
嘉瓦 <104463355+renzudadi@users.noreply.github.com>
2022-06-07 17:54:58 +0100

    Update currency(new).py
    debug

--- currency(new).py
+++ currency(new).py
@@ -1,5 +1,9 @@
 import pandas as pd
 import ssl
+import datetime
+time1 = datetime.datetime.now()
+time1 = str(time1)
+time1 = time1[:16]
 ssl._create_default_https_context = ssl._create_unverified_context
 
 df = pd.read_html("https://www.boc.cn/sourcedb/whpj/")
@@ -11,5 +15,5 @@ df = df[0]
 currency = df.loc[7]
 text = str(currency)
 text = text.split()
-cny_currency  = "中国银行英镑"+"汇率"+" "+str(float(text[9])/100)
+cny_currency  = "当前中国银行英镑"+"汇率"+" "+str(float(text[9])/100) + "\n"+"英国时间"+" " + str(time1)
 print(cny_currency)
